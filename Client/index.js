0c089721632e4144995367d058f206800e420dff
Bogdan Blaga <bblaga@macadamian.com>
2016-04-09 21:56:26 +0300

    Try 22

--- Client/index.js
+++ Client/index.js
@@ -18,24 +18,24 @@ function startDrone(){
 
 
 	setInterval(function(){
-	  //   $.ajax({ url: "http://40.76.51.174/get_status", 
-	  //   	success: function(data){
-		 //        //Update your dashboard gauge
-		 //        console.log(data);
-		 //        $("#progressbar").attr('style', 'width:'+data+'%');
-		 //        $("#progressbar").attr('aria-valuenow', data);
-		 //    },
-		 //    xhrFields: {
-			//     // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-			//     // This can be used to set the 'withCredentials' property.
-			//     // Set the value to 'true' if you'd like to pass cookies to the server.
-			//     // If this is enabled, your server must respond with the header
-			//     // 'Access-Control-Allow-Credentials: true'.
-			//     withCredentials: false
-			// },
-		 //    beforeSend: function(xhr){xhr.setRequestHeader('Access-Control-Allow-Origin', '*');},
-		 //    dataType : "json"});
-		 makeCorsRequest('GET', "http://40.76.51.174/get_status");
+	    $.ajax({ url: "http://40.76.51.174/get_status", 
+	    	success: function(data){
+		        //Update your dashboard gauge
+		        console.log(data);
+		        $("#progressbar").attr('style', 'width:'+data+'%');
+		        $("#progressbar").attr('aria-valuenow', data);
+		    },
+		    xhrFields: {
+			    // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+			    // This can be used to set the 'withCredentials' property.
+			    // Set the value to 'true' if you'd like to pass cookies to the server.
+			    // If this is enabled, your server must respond with the header
+			    // 'Access-Control-Allow-Credentials: true'.
+			    withCredentials: false
+			},
+		    beforeSend: function(xhr){xhr.setRequestHeader('Access-Control-Allow-Origin', '*');},
+		    dataType : "json"});
+		 // makeCorsRequest('GET', "http://40.76.51.174/get_status");
 	}, 2000);
 }
 
