61c001398ba0f8119c9a27cb3e54c7cad807c358
Bogdan Blaga <bblaga@macadamian.com>
2016-04-09 21:18:00 +0300

    more cars tryouts

--- Client/index.js
+++ Client/index.js
@@ -25,6 +25,14 @@ function startDrone(){
 		        $("#progressbar").attr('style', 'width:'+data+'%');
 		        $("#progressbar").attr('aria-valuenow', data);
 		    },
+		    xhrFields: {
+			    // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+			    // This can be used to set the 'withCredentials' property.
+			    // Set the value to 'true' if you'd like to pass cookies to the server.
+			    // If this is enabled, your server must respond with the header
+			    // 'Access-Control-Allow-Credentials: true'.
+			    withCredentials: false
+			},
 		    beforeSend: function(xhr){xhr.setRequestHeader('Access-Control-Allow-Origin', '*');},
 		    dataType : "json"});
 	}, 2000);
